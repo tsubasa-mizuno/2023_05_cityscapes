a33cc50a1a21c2c29b494686e8450cef408de50f
tsubasa mizuno <t.mizuno.384@nitech.jp>
2023-05-09 11:24:42 +0900

    20230509

--- args.py
+++ args.py
@@ -84,7 +84,7 @@ def get_args():
     #     type=int,
     #     default=16
     # )
-    parser.add_argument("--crop_size", type=int, default=256)
+    parser.add_argument("--crop_size", type=int, default=512)
 
     # parser.add_argument(
     #     '--num_workers',
