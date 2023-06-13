ca5e89668901c543f0b4f685365ab41ce4568811
tsubasa mizuno <t.mizuno.384@nitech.jp>
2023-06-13 13:52:41 +0900

    20230613

--- train.py
+++ train.py
@@ -32,6 +32,7 @@ def train(
     model.train()
 
     train_loss = AverageMeter()
+    i = 0
 
     with tqdm(loader, leave=False) as pbar_train:
         pbar_train.set_description("[train]")
@@ -45,57 +46,39 @@ def train(
             labels = labels.squeeze(dim=1)  # B, H, W
             bs = len(image)
             # labels_shape[8, 256, 512]
+
             if args.model == "Mask2Former":
                 for i in range(bs):
                     image[i] = Image.open(image[i])
+
                 inputs = processor(images=image, return_tensors="pt")
                 inputs["pixel_values"] = inputs["pixel_values"].to(device)
                 inputs["pixel_mask"] = inputs["pixel_mask"].to(device)
-                # mask_labels_2 = torch.stack(
-                #     [(labels == i).long() for i in range(args.num_class - 1)]
-                # )
-                # mask_labels_2 = mask_labels_2.long()
-
-                # グランドトゥルースオブジェクトの数を取得
-                labels = labels.long()  # B, H, W: int64
-                num_target_boxes = labels.max() + 1
 
                 class_labels = []
                 mask_labels = []
+                for b in range(bs):
+                    class_label_of_b = labels[b].unique()
+                    class_labels.append(class_label_of_b)  # list of [num_labels] long?
 
-                # 各オブジェクトに対して処理を行う
-                for i in range(num_target_boxes):
-                    # オブジェクトの領域を特定
-                    mask = labels == i  # B, H, W: bool
+                    # 各カテゴリiのmaskに対して処理を行う
+                    mask_labels_of_b = []
+                    for i in class_label_of_b:
+                        mask = labels[b] == i  # H, W: bool
+                        mask_labels_of_b.append(mask.float())  # list of [H, W] float
+                    mask_labels_of_b = torch.stack(mask_labels_of_b)
 
-                    # マスクが空でないことを確認してからクラスラベルを取得
-                    if mask.any():
-                        class_label = labels * mask  # B, H, W: int64
-                    else:
-                        # Handle the case when the mask is empty
-                        class_label = None
-
-                    # オブジェクトのマスクを保存
-                    mask_labels.append(mask)
-
-                    # オブジェクトのクラスラベルを保存
-                    class_labels.append(class_label)
-
-                # Noneの値をフィルタリングする
-                class_labels = [label for label in class_labels if label is not None]
-
-                # 結果をテンソルに変換
-                class_labels = (
-                    torch.stack(class_labels)  # list of [B, H, W] to (N, B, H, W)
-                    .reshape(len(class_labels), bs, -1)  # (N, B, HW)
-                    .max(dim=2)  # (N, B)
-                    .values
-                )
-                mask_labels = torch.stack(mask_labels).float()
+                    mask_labels.append(
+                        mask_labels_of_b
+                    )  # list of [num_labels, H, W] float
 
                 outputs = model(
                     **inputs, class_labels=class_labels, mask_labels=mask_labels
                 )
+                # target = processor.post_process_semantic_segmentation(
+                #     outputs, target_sizes=[image[0].size[::-1]]
+                # )[0]
+
                 loss = outputs.loss
                 # target_shape[256, 512]
 
@@ -105,7 +88,8 @@ def train(
                 loss = criterion(target, labels.long())
                 # target_shape[8, 3, 256, 512]
 
-            # imagesave(target, labels, args, 3, epoch)
+            # imagesave(target, labels, args, i, epoch)
+            i += 1
             train_loss.update(loss, labels.size(0))
             optimizer.zero_grad()
             loss.backward()
